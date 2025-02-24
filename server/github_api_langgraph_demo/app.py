791fa32702de3f0ef0059dacdbb0225a45e15978
Yason Khaburzaniya <yason@hey.com>
2025-02-24 09:49:45 -0800

    add comments for presentation

--- server/github_api_langgraph_demo/app.py
+++ server/github_api_langgraph_demo/app.py
@@ -10,31 +10,28 @@ from langgraph.prebuilt import create_react_agent
 from pydantic import BaseModel
 
 
+# Have to setup a pydantic model for structured output, otherwise have to manually explain format to the Agent
 class Author(BaseModel):
     """Respond to the user in this format."""
     name: str
     issue_count: int
     comment_count: int
 
-# Load environment variables from .env
 load_dotenv()
-
 
 # Initialize GitHub API Wrapper
 github = GitHubAPIWrapper()
 
-# Instantiate GitHub Toolkit
+
+# Toolkit is pre-programmed api wrapper for GitHub sdk. Why does it need to be pre-programmed?
 toolkit = GitHubToolkit.from_github_api_wrapper(github)
 
-# View available tools
-all_tools = toolkit.get_tools()
-for tool in all_tools:
-    print(tool.name)
 
 llm = ChatOpenAI(model="gpt-4o", temperature=0)
 
 tools = [setattr(tool, "name", tool.mode) or tool for tool in toolkit.get_tools()]
 
+# ReAct agent, a hybrid deliberative and reactive agent
 agent_executor = create_react_agent(llm, tools, response_format=Author)
 
 app = Flask(__name__)
@@ -42,7 +39,7 @@ CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 @app.route('/data', methods=['GET'])
 def get_data():
-    # example_query = "Who is the most prolific author of issues. Respond in JSON with author_name as the field. Don't use markdown, your output will be sent to a software program to use. If making a function call, provide explanation for the function call. "
+    # Very limiting, has to conform to the model and available tools. Underwhelming experience.
     example_query = ("For the most prolific author of github issues, give me the number of issues they have created. "
                      "Given the author's issues, I want to know the number of comments that same author has made." )
 
@@ -54,6 +51,7 @@ def get_data():
     for event in events:
         event["messages"][-1].pretty_print()
 
+    # Structured output is a nice breakthrough, previously getting JSON output was not consistent
     structured_response = event.get("structured_response")
     data = {"data": [structured_response.model_dump()]}
     print(data)
